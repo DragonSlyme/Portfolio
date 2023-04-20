ef5e168788fee3eef28977e75f03c8fb4ecd03ae
DragonSlyme <dragonslyme@protonmail.com>
2023-04-20 23:49:37 +0200

    Correct bug in test back-end

--- message.js
+++ message.js
@@ -4,8 +4,9 @@ const statutTxt = form.querySelector('.submit span');
 form.onsubmit = (e)=>{
     e.preventDefault(); // Prevent form from submitting
     statutTxt.style.display = 'block'; // Show status text
+
     let xhr = new XMLHttpRequest(); // Create XML object
-    xhr.open("POST", "php/form.php", true);
+    xhr.open("POST", "form.php", true);
     xhr.onload = ()=>{
         if(xhr.readyState === XMLHttpRequest.DONE){
             if(xhr.status === 200){
